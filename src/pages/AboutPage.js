6c3280d9cc3e852539fd79f0a367151a60267daa
Jayanthbharadwaj <jayanthbharadwajm@gmail.com>
2022-01-22 21:19:27 +0530

    Changes to About Page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,4 +1,4 @@
-import { Layout, Skeleton } from 'antd';
+import { Layout, Skeleton, Button } from 'antd';
 
 import FooterComponent from '../components/FooterComponent';
 import MainHeader from '../components/MainHeader';
@@ -11,6 +11,7 @@ export default function AboutPage() {
       <MainHeader active={'2'} />
       <Content style={{ padding: '0 50px' }}>
         <div className='site-layout-content'>
+          <Button type='primary'>About Us</Button>
           <Skeleton active paragraph={{ rows: 30 }} />
         </div>
       </Content>
